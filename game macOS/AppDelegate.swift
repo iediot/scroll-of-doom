2d2426c987d5435cba8e2a8a4c7efc9059efe746
iediot <eduardnicolae092@gmail.com>
2026-07-02 23:23:30 +0300

    Initial Commit

---
+++ game macOS/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  game macOS
+//
+//  Created by Tabirca Nicolae-Eduard on 02/07/2026.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
